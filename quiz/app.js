65607f887fc37205848d8a0b68476e4a96d80e57
codingwolf-at <atameshwari9@gmail.com>
2021-09-13 23:46:58 +0530

    FINISHED: quiz module done

---
+++ quiz/app.js
@@ -0,0 +1,20 @@
+const quizForm = document.querySelector("#triangle-quiz");
+const submitForm = document.querySelector("#submit-btn");
+const outputHeading = document.querySelector("#output-heading");
+
+const correctAnswers = ['90', 'right-angled']; 
+
+const calculateScore = () => {
+  let score = 0;
+  let index = 0;
+  const data = new FormData(quizForm);
+  for (let answer of data.values()) {
+    if (answer === correctAnswers[index]) {
+      score = score + 1
+    }
+    index = index + 1;
+  }
+  outputHeading.innerText = `You score is : ${score}`;
+}
+
+submitForm.addEventListener("click", calculateScore)
