8df29f1dd12db9ac9ab2171c047b37f898c75984
Atul Tameshwari <atameshwari9@gmail.com>
2022-10-08 14:12:45 +0000

    refac: added validations

--- app.js
+++ app.js
@@ -5,10 +5,19 @@ const outputHeading = document.querySelector("#output-heading");
 isTriangleButton.addEventListener("click", isTriangleHandler);
 
 function isTriangleHandler() {
-  const sum = calculateSumOfAngles(angleInput);
-  sum === 180
-  ? outputHeading.innerText = "Yayy, the angles form a triangle"
-  : outputHeading.innerText = "Oh Oh!, the angles do not form a triangle"
+  if (Number(angleInput[0].value) < 0 
+    || Number(angleInput[1].value) < 0 
+    || Number(angleInput[2].value) < 0
+    || angleInput[0].value === ""
+    || angleInput[1].value === ""
+    || angleInput[2].value === "") {
+    alert('All angles must be a positive non zero number');
+  } else {
+    const sum = calculateSumOfAngles(angleInput);
+    sum === 180
+    ? outputHeading.innerText = "Yayy, the angles form a triangle"
+    : outputHeading.innerText = "Oh Oh!, the angles do not form a triangle"
+  }
 }
 
 function calculateSumOfAngles(angleInput) {
