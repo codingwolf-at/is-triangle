13a17c7f3a8fb11e4c22e0d1c63d711e48ac1305
Atul Tameshwari <atameshwari9@gmail.com>
2022-10-08 14:56:35 +0000

    refac: added more questions

--- quiz/app.js
+++ quiz/app.js
@@ -2,7 +2,7 @@ const quizForm = document.querySelector("#triangle-quiz");
 const submitForm = document.querySelector("#submit-btn");
 const outputHeading = document.querySelector("#output-heading");
 
-const correctAnswers = ['90', 'right-angled']; 
+const correctAnswers = ['90', 'right-angled', 'isosceles', 'Scalene', '360']; 
 
 const calculateScore = () => {
   let score = 0;
